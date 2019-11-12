0859330f1b2d4f9398f9a951ac7f294e8058c5c4
Sivan Mehta <Sivanmehta2@gmail.com>
2019-11-11 20:21:38 -0800

    it narrowly works

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ yourself, run the following commmands
 make reset
 node forge.js
 ```
+
+# TODO
+
+- arbitrary number of commits
+- preserve commit messages
+- take start as a CLI flag and evenly distribute commits from then till now
--- forge.js
+++ forge.js
@@ -2,54 +2,64 @@ const { exec } = require('child_process');
 
 const ONE_DAY = 1000 * 60 * 60 * 24;
 const today = new Date();
-// 100 days ago will be our starting point
-const days = 100;
-const start = new Date(+today - ONE_DAY * days);
 
-function rebuildStack(size = 1) {
-  exec('git stash pop', () => {
-    const day = new Date(+start + ONE_DAY * size).toUTCString();
-    const commit = `GIT_COMMITTER_DATE="${day}" git commit -m "add data/${size}.txt" --no-edit --date "${day}"`
-    exec(commit, () => {
-      exec('git stash list | wc -l', (err, stdout) => {
-        const completed = days - parseInt(stdout.match(/[0-9]+/)[0]);
-        if(completed < days) {
-          rebuildStack(completed + 1);
-        }
+class Runner {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.days = 100;
+    this.beginning = new Date(+today - ONE_DAY * this.days);
+  }
+
+  start() {
+    // gather list of commmits
+    exec('git log | grep commit', (error, stdout, stderr) => {
+      const shas = stdout
+        .split('\n')
+        .map(line => line.replace(/commit /, ''))
+        .filter(line => line.length > 0);
+    
+      // send to be reset
+      this.buildStack(shas.length)
+    });
+  }
+
+  /**
+   * For each given sha, reset all the way back to the beginning, storing everything
+   * in git's stash stack
+   * @param {Number} idx - which text file we're reverting 
+   */
+  buildStack(idx) {
+    exec('git reset --soft HEAD~', () => {
+      exec('git stash', () => {
+        if(idx > 1) {
+          this.buildStack(idx - 1);
+        } else {
+          this.rebuildStack();
+        }        
       });
     });
-  });
-}
+  }
 
-
-/**
- * For each given sha, reset all the way back to the beginning, storing everything
- * in git's stash stack
- * @param {Number} idx - which text file we're reverting 
- */
-function buildStack(idx) {
-  exec('git reset --soft HEAD~', () => {
-    exec('git stash', () => {
-      if(idx > 1) {
-        buildStack(idx - 1);
-      } else {
-        rebuildStack();
-      }        
+  rebuildStack(size = 1) {
+    // get the commit log before stashing
+    // git log -1 --pretty=%B
+    exec('git stash pop', () => {
+      const day = new Date(+this.beginning + ONE_DAY * size).toUTCString();
+      const commit = `GIT_COMMITTER_DATE="${day}" git commit -m "add data/${size}.txt" --no-edit --date "${day}"`
+      exec(commit, () => {
+        exec('git stash list | wc -l', (err, stdout) => {
+          const completed = this.days - parseInt(stdout.match(/[0-9]+/)[0]);
+          if(completed < this.days) {
+            this.rebuildStack(completed + 1);
+          }
+        });
+      });
     });
-  });
+  }
 }
 
-function run() {
-  // gather list of commmits
-  exec('git log | grep commit', (error, stdout, stderr) => {
-    const shas = stdout
-      .split('\n')
-      .map(line => line.replace(/commit /, ''))
-      .filter(line => line.length > 0);
-  
-    // send to be reset
-    buildStack(shas.length)
-  });
-}
-
-run();
+const runner = new Runner();
+runner.start();
