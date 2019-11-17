369a35bf0877f890e7a8038b76bde03faed4fde3
Sivan Mehta <Sivanmehta2@gmail.com>
2019-11-17 11:08:31 -0800

    async/await because callback trees are dumb (#1)

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
 ##  `git-schmear`
 
-Spread the existing commits over a timeframe.
+Spread the existing commits over a timeframe. Use this to pretend like you've
+been working this whole time, rather than cramming in everything at the last
+minute.
 
 ## Running yourself
 
-```
+```sh
 cd /repo/you/want/to/spoof/
 cp /path/to/this/repo/forge.js .
 node forge.js
 ```
 
-If you want to spoof a fresh repo, try running `spoof.sh` to get yourself some
-freshly minted commits. You can see [this](https://github.com/SivanMehta/git-schmeared)
+If you want to spoof a fresh repo, copy over `spoof.sh` to get yourself some
+freshly minted commits. You can see [this](schmeared)
 repo for an example of a repo that has been `git-schmeared`.
-
-## Features
-
-- Preserves commit messages
 
 ## TODO
 
-- evenly distributing commits over a given time frame.
-- take start as a CLI flag and evenly distribute commits from then till now
+- ~~literally any testing~~
+- Variable intervals of schmear, as opposed to 1 day intervals.
+- Preserving `git` authorship, which is currently blown away.
+- Take start time as a CLI parameter and distribute commits from then until now.
+
+[schmeared]: https://github.com/SivanMehta/git-schmeared
--- forge.js
+++ forge.js
@@ -1,71 +1,108 @@
-const { exec } = require('child_process');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
 
 const ONE_DAY = 1000 * 60 * 60 * 24;
 const today = new Date();
+
+/**
+ * Form a git commit message given an actual message and a date on which we're
+ * pretending the commit happened.
+ *
+ * @param  {String} message - String of what the commit originally was
+ * @param  {String} day - When the commit "happened"
+ * @return {String} formatted `git commit` command
+ */
+function formCommit(message, day) {
+  return `GIT_COMMITTER_DATE="${day}" git commit -m "${message}" --date "${day}"`;
+}
 
 class Runner {
   constructor() {
     this.messageStack = [];
   }
 
-  start() {
-    exec('git log --oneline', (error, stdout, stderr) => {
-      const shas = stdout
-        .split('\n')
-        .map(line => line.replace(/commit /, ''))
-        .filter(line => line.length > 0);
+  /**
+   * Actually run the forgery.
+   * @async
+   */
+  async start() {
+    const { stdout } = await exec('git log --oneline');
 
-      this.days = shas.length;
-      this.beginning = new Date(+today - ONE_DAY * this.days);
+    const shas = stdout
+      .split('\n')
+      .map(line => line.replace(/commit /, ''))
+      .filter(line => line.length > 0);
 
-      this.buildStack(shas.length);
-    });
+    this.days = shas.length;
+    this.beginning = new Date(+today - ONE_DAY * this.days);
+
+    await this.buildStack(shas.length);
   }
 
   /**
-   * For each given sha, reset all the way back to the beginning, storing everything
-   * in git's stash stack
-   * @param {Number} idx - which text file we're reverting
+   * Form a `git commit` compatible date string that represents the given number
+   * of days after we started.
+   *
+   * @param  {Number} after - how many days after the start
+   * @returns {String} date string
+   * @async
    */
-  buildStack(idx) {
-    exec('git log --oneline -1 --pretty=%B', (err, stdout) => {
-      const lines = stdout.split('\n');
-      const message = lines.slice(0, lines.length - 2).join('\n');
-
-      exec('git reset --soft HEAD~', () => {
-        exec('git stash', () => {
-          if(idx > 1) {
-            this.messageStack.push(message);
-            this.buildStack(idx - 1);
-          } else {
-            this.rebuildStack(idx);
-          }
-        });
-      });
-    });
+  formDay(after) {
+    return new Date(+this.beginning + ONE_DAY * after).toUTCString();
   }
 
   /**
-   * Using the built up git stash, rebuild every commit, but with a new date
-   * @param  {Number} size How many commits we have built so far
+   * For each given sha, reset all the way back to the beginning, storing
+   * everything in git's stash.
+   *
+   * @param {Number} idx - index in the commit stack
+   * @async
    */
-  rebuildStack(size) {
-    exec('git stash pop', () => {
-      const day = new Date(+this.beginning + ONE_DAY * size).toUTCString();
-      const message = this.messageStack.pop();
-      const commit = `GIT_COMMITTER_DATE="${day}" git commit -m "${message}" --date "${day}"`;
+  async buildStack(idx) {
+    const { stdout } = await exec('git log --oneline -1 --pretty=%B');
+    const lines = stdout.split('\n');
+    const message = lines.slice(0, lines.length - 2).join('\n');
 
-      exec(commit, () => {
-        exec('git stash list | wc -l', (err, stdout) => {
-          const completed = this.days - parseInt(stdout.match(/[0-9]+/)[0]);
-          if(this.messageStack.length > 0) {
-            this.rebuildStack(completed + 1);
-          }
-        });
-      });
-    });
+    // The first commit is a special case because we cannot `git reset`.
+    if (idx == 1) {
+      await exec('git update-ref -d HEAD');
+      const day = this.formDay(0);
+      const commit = formCommit(message, day);
+
+      await exec(commit);
+      await this.rebuildRepo(idx);
+    } else {
+      await exec('git reset --soft HEAD~');
+      await exec('git stash');
+
+      this.messageStack.push(message);
+      await this.buildStack(idx - 1);
+    }
+  }
+
+  /**
+   * Using the built up git stash, rebuild every commit, but with a new dates 😈.
+   *
+   * @param  {Number} size - how many commits we have built so far
+   * @async
+   */
+  async rebuildRepo(size) {
+    await exec('git stash pop');
+
+    const day = this.formDay(size);
+    const message = this.messageStack.pop();
+    const commit = formCommit(message, day);
+    await exec(commit);
+
+    if(this.messageStack.length > 0) {
+      const { stdout } = await exec('git stash list | wc -l');
+      const completed = this.days - parseInt(stdout.match(/[0-9]+/)[0]);
+      await this.rebuildRepo(completed + 1);
+    }
   }
 }
 
-const runner = new Runner();
-runner.start();
+(async function () {
+  const runner = new Runner();
+  await runner.start();
+})();
